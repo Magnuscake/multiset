b16b2b62e01c49a86301ede1b4ae95be836605ba
Magnuscake <saneyar.k@gmail.com>
2020-08-20 20:17:07 +1000

    Completed OrderedLinkedList add, print, search, searchByInstance and contains

--- implementation/OrderedLinkedListMultiset.java
+++ implementation/OrderedLinkedListMultiset.java
@@ -1,5 +1,6 @@
 package implementation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,41 +11,148 @@ import java.util.List;
  */
 public class OrderedLinkedListMultiset extends RmitMultiset
 {
+    // Reference to the head node 
+    protected Node head;
+    // Reference to the length of list
+    protected int length;
 
+    public OrderedLinkedListMultiset() {
+        head = null;
+        length = 0;
+    }
+    
     @Override
 	public void add(String item) {
-        // Implement me!
+        Node newNode = new Node(item);
+
+        if (head == null) {
+            head = newNode;
+
+        } else {
+            Node prevNode = null;
+            Node currNode = head;
+
+            boolean createNewItem = true;
+
+            while (currNode != null) {
+                if (currNode.getItem().equals(item)) {
+                    currNode.setInstances(currNode.getInstances() + 1); 
+                    
+                    Node nextNode = currNode.getNextNode();
+                    while (nextNode != null) {
+                        if (currNode.getInstances() > nextNode.getInstances()) {
+                            // swap currNode and nextNode
+                            if (prevNode != null) {
+                                prevNode.setNextNode(nextNode);
+                                // Specify the new head
+                            } else head = nextNode;
+                            currNode.setNextNode(nextNode.getNextNode());
+                            nextNode.setNextNode(currNode);
+                        } else break;
+
+                        prevNode = nextNode;
+                        nextNode = currNode.getNextNode();
+                    }
+                    createNewItem = false;
+               }
+
+               prevNode = currNode;
+               currNode = currNode.getNextNode();
+            }
+
+            if (createNewItem) {
+                newNode.setNextNode(head);
+                head = newNode;
+            }
+            length++;
+
+            
+        } 
     } // end of add()
 
 
     @Override
 	public int search(String item) {
-        // Implement me!
+        Node currNode = head;
 
-        // Placeholder, please update.
+        while (currNode != null) {
+            if (currNode.getItem().compareTo(item) == 0)
+                return currNode.getInstances();
+
+            currNode = currNode.getNextNode();
+        }
+        
         return searchFailed;
     } // end of search()
 
 
     @Override
 	public List<String> searchByInstance(int instanceCount) {
+        List<String> matchItems = new ArrayList<>();
+        Node currNode = head;
 
-        // Placeholder, please update.
-        return null;
+        while (currNode != null) {
+            if (currNode.getInstances() == instanceCount) {
+                matchItems.add(currNode.getItem());
+            }
+            currNode = currNode.getNextNode();
+        }
+        return matchItems;
     } // end of searchByInstance
 
 
     @Override
 	public boolean contains(String item) {
-        // Implement me!
+        Node currNode = head;
 
-        // Placeholder, please update.
+        while (currNode != null) {
+            if (currNode.getItem().compareTo(item) == 0) {
+                return true;
+            }
+        }
         return false;
     } // end of contains()
 
 
     @Override
 	public void removeOne(String item) {
+        // TODO: order element as and when they are removed
+        if (head == null) {
+            return;
+        }
+
+        Node currNode = head;
+        Node prevNode = null;
+
+        while (currNode != null) {
+            if (currNode.getItem().compareTo(item) == 0) {
+                currNode.setInstances(currNode.getInstances() - 1);
+
+                // delete item when count is 0
+                if (currNode.getInstances() == 0) {
+                    // length of 1 means only head is remaining
+                    if (length == 1) {
+                        head = null;
+                    } 
+                    else if (currNode == head) {
+                        head = currNode.getNextNode();
+                        currNode = null;
+                        return;
+                    }
+
+                    currNode = null;
+                } else {
+                    while (currNode != null) {
+                        // Order list logic
+                        break;
+                    }
+                }
+            }
+
+            prevNode = currNode;
+            currNode = currNode.getNextNode();
+        }
+        
         // Implement me!
     } // end of removeOne()
 
@@ -52,14 +160,31 @@ public class OrderedLinkedListMultiset extends RmitMultiset
     @Override
 	public String print() {
 
-        // Placeholder, please update.
-        return new String();
+        Node currNode = head;
+
+        StringBuffer strList = new StringBuffer();
+
+        while (currNode != null) {
+            strList.append(currNode.getItem() + ":" + currNode.getInstances() + "\n");
+            currNode = currNode.getNextNode();
+        }
+
+        return strList.toString();
     } // end of OrderedPrint
 
 
     @Override
 	public String printRange(String lower, String upper) {
+        Node currNode = head;
 
+        StringBuffer rangeList = new StringBuffer();
+
+        while (currNode != null) {
+            if (currNode.getItem().compareTo(lower) >= 0 && currNode.getItem().compareTo(upper) <= 0) {
+                rangeList.append(currNode);
+            }
+            currNode = currNode.getNextNode();
+        }
         // Placeholder, please update.
         return new String();
     } // end of printRange()
@@ -89,3 +214,35 @@ public class OrderedLinkedListMultiset extends RmitMultiset
     } // end of difference()
 
 } // end of class OrderedLinkedListMultiset
+
+class Node {
+    protected String _item;
+    private Node _nextNode;
+    private int _instances;
+
+    public Node(String item) {
+        _item = item;
+        _nextNode = null;
+        _instances = 1;
+    }
+
+    public String getItem() {
+        return _item;
+    }
+
+    public Node getNextNode() {
+        return _nextNode;
+    }
+
+    public int getInstances() {
+        return _instances;
+    }
+
+    public void setNextNode(Node nextNode) {
+        _nextNode = nextNode;
+    }
+
+    public void setInstances(int instances) {
+        _instances = instances;
+    }
+}
